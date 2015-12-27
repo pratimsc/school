f1bd46a06ac72bd8d4aef30cc27edf5d9cd0a613
Pratim SC <pratimsc@voyager.bhrahmand>
2015-12-27 23:45:47 +0000

    Initial commit

---
+++ app/controllers/SchoolController.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import play.api.mvc.Controller
+
+/**
+  * Created by pratimsc on 27/12/15.
+  */
+class SchoolController extends  Controller{
+
+  def findById(id:Long)= {
+    TODO
+  }
+
+  def addSchool = TODO
+
+}
---
+++ app/controllers/StudentController.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import play.api.mvc.Controller
+
+/**
+  * Created by pratimsc on 27/12/15.
+  */
+class StudentController extends Controller{
+
+  def findById(id:Long, school:Long) = TODO
+
+  def addStudent = TODO
+
+}
